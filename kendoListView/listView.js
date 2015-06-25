10d9071ec69a75a01ffaa4b656fc22cc4dcd3339
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2015-06-25 14:51:42 +0300

    Update list

--- kendoListView/listView.js
+++ kendoListView/listView.js
@@ -1,10 +1,35 @@
-var listView = Backbone.View.extend({
+$(function() {
+  var listsView = Backbone.View.extend({
+    el: $("#listRender"),
 
-  tagName: "div",
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.collection = new listCollection();
+      this.collection.add(mike);
+      this.collection.add(jack);
+      this.collection.add(russel);
+      var products = this.collection.toJSON();
+      var dataSource = new kendo.data.DataSource({
+        data: products,
+        pageSize: 2
+      });
+      dataSource.read();
+      this.render(dataSource);
+    },
 
-  render: function() {
-    var listItemTemplate = _.template($("#listItem").html());
-    this.$el.html(listItemTemplate(this.model.toJSON()));
-    return this;
-  }
+    render: function(dataSource) {
+      console.log(dataSource);
+
+      this.$el.kendoListView({
+        dataSource: dataSource,
+        template: kendo.template($("#listItem").html())
+      });
+
+      $("#pager").kendoPager({
+        dataSource: dataSource
+      });
+    }
+  });
+
+  new listsView();
 });
