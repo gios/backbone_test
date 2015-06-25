7ca52a4c484233643d3f7e09919c78d86220b352
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2015-06-25 18:02:26 +0300

    Relocate defaults data to other file

---
+++ kendoListView/dataDefault.js
@@ -0,0 +1,121 @@
+var dataDefault = [{
+  name: "Mike",
+  age: 18,
+  salary: 2100
+}, {
+  name: "Jack",
+  age: 20,
+  salary: 2700
+}, {
+  name: "Russel",
+  age: 22,
+  salary: 2300
+}, {
+  name: "Pickachu",
+  age: 12,
+  salary: 2500
+}, {
+  name: "Jessy",
+  age: 21,
+  salary: 1200
+}, {
+  name: "Mike",
+  age: 18,
+  salary: 2100
+}, {
+  name: "Jack",
+  age: 20,
+  salary: 2700
+}, {
+  name: "Russel",
+  age: 22,
+  salary: 2300
+}, {
+  name: "Pickachu",
+  age: 12,
+  salary: 2500
+}, {
+  name: "Jessy",
+  age: 21,
+  salary: 1200
+}, {
+  name: "Mike",
+  age: 18,
+  salary: 2100
+}, {
+  name: "Jack",
+  age: 20,
+  salary: 2700
+}, {
+  name: "Russel",
+  age: 22,
+  salary: 2300
+}, {
+  name: "Pickachu",
+  age: 12,
+  salary: 2500
+}, {
+  name: "Jessy",
+  age: 21,
+  salary: 1200
+}, {
+  name: "Mike",
+  age: 18,
+  salary: 2100
+}, {
+  name: "Jack",
+  age: 20,
+  salary: 2700
+}, {
+  name: "Russel",
+  age: 22,
+  salary: 2300
+}, {
+  name: "Pickachu",
+  age: 12,
+  salary: 2500
+}, {
+  name: "Jessy",
+  age: 21,
+  salary: 1200
+}, {
+  name: "Mike",
+  age: 18,
+  salary: 2100
+}, {
+  name: "Jack",
+  age: 20,
+  salary: 2700
+}, {
+  name: "Russel",
+  age: 22,
+  salary: 2300
+}, {
+  name: "Pickachu",
+  age: 12,
+  salary: 2500
+}, {
+  name: "Jessy",
+  age: 21,
+  salary: 1200
+}, {
+  name: "Mike",
+  age: 18,
+  salary: 2100
+}, {
+  name: "Jack",
+  age: 20,
+  salary: 2700
+}, {
+  name: "Russel",
+  age: 22,
+  salary: 2300
+}, {
+  name: "Pickachu",
+  age: 12,
+  salary: 2500
+}, {
+  name: "Jessy",
+  age: 21,
+  salary: 1200
+}];
